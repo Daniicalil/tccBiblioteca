a29994ac4845534039823b3960f2a021c0dc8bf2
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-05-16 22:39:17 -0300

    up

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // import LognIn from './src/LognIn/index';
-// import SignUp from './src/signUp';
+import SignUp from './src/signUp';
 // import EsqueceuSenha1 from './src/esquec1/index';
-import EsqueceuSenha2 from './src/esquec2/index';
+// import EsqueceuSenha2 from './src/esquec2/index';
 // import TelaInicial from './src/telaInicial/index';
 // import Perfil from './src/perfil/index';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <EsqueceuSenha2 />
+      <SignUp />
     </SafeAreaProvider>
   );
 }
