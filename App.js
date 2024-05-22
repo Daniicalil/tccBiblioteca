3f23e8c8901d0a0413285ec57116a3af70eec4c0
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-05-22 20:27:52 -0300

    up

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'; 
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 
 // import LognIn from './src/LognIn/index';
 // import SignUp from './src/signUp';
 // import EsqueceuSenha1 from './src/esquec1/index';
 // import EsqueceuSenha2 from './src/esquec2/index';
-import TelaInicial from './src/telaInicial/index';
-// import Perfil from './src/perfil/index';
+// import TelaInicial from './src/telaInicial/index';
+import Perfil from './src/perfil/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TelaInicial /> 
+      <Perfil /> 
     </View>
   );
 }
