5135c0b1fe1509098faa17721523b65df6e675ea
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-06-07 20:37:58 -0300

    up

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { StyleSheet, Text, View } from 'react-native';
 // import Perfil from './src/ALUNO/perfil';
 // import Recomendacao from './src/ALUNO/recomendacao';
 // import InfoLivroRecomendacao from './src/ALUNO/infoLivroRecomendacao';
-// import Biblioteca from './src/ALUNO/biblioteca';
+import Biblioteca from './src/ALUNO/biblioteca';
 // import InfoLivroBiblioteca from './src/ALUNO/infoLivroBiblioteca';
 // import InformacoesReserva from './src/ALUNO/informacoesReserva';
 // import Notificacoes from './src/ALUNO/notificacoes';
@@ -17,21 +17,6 @@ import { StyleSheet, Text, View } from 'react-native';
 // import ReservarLivro from './src/ALUNO/reservarLivro';
 // import MainNavigation from './src/ALUNO/sidebar';
 
-// import LognIn from './src/PROFESSOR/lognIn';
-// import SignUp from './src/PROFESSOR/signUp';
-// import EsqueceuSenha1 from './src/PROFESSOR/esqueceuSenha1';
-// import EsqueceuSenha2 from './src/PROFESSOR/esqueceuSenha2';
-// import TelaInicial from './src/PROFESSOR/telaInicial';
-// import Perfil from './src/PROFESSOR/perfil';
-// import Recomendacao from './src/PROFESSOR/recomendacao';
-// import InfoLivroRecomendacao from './src/PROFESSOR/infoLivroRecomendacao';
-// import Biblioteca from './src/PROFESSOR/biblioteca'; 
-// import InfoLivroBiblioteca from './src/PROFESSOR/infoLivroBiblioteca';
-// import InformacoesReserva from './src/PROFESSOR/informacoesReserva';
-// import Notificacoes from './src/PROFESSOR/notificacoes';
-// import InformacoesContato from './src/PROFESSOR/infoContato';
-// import ReservarLivro from './src/PROFESSOR/reservarLivro';
-import MainNavigation from './src/PROFESSOR/sidebar';
 
 // import Navegacao from './src/ALUNO/rotas';
 // import Navegacao from './src/PROFESSOR/rotas';
@@ -39,7 +24,7 @@ import MainNavigation from './src/PROFESSOR/sidebar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <MainNavigation /> 
+      <Biblioteca /> 
     </View>
   );
 }
